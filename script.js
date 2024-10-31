e1ab8d2806a3730750749e090b167fd3a4e50d9d
Alvesl9 <gregorylinhares99@gmail.com>
2024-10-31 17:24:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Aqui podemos adicionar interatividade, como um modal para fotos ou um player de música.
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Script loaded and ready!");
+  });
+  
